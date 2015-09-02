4c8620aac2773bf5b901acee0912190e4f89fc6f
ChalkPE <amato0617@gmail.com>
2015-09-02 18:49:05 +0900

    Added Group to main page

--- index.php
+++ index.php
@@ -3,10 +3,22 @@
     <head>
         <meta charset="utf-8"> <title>ChalkPE</title>
         <link rel="stylesheet" type="text/css" href="/src/css/style.css" />
+        <style media="screen">
+            html, body {
+                overflow: auto;
+            }
+
+            .preview {
+                display: inline-block;
+                vertical-align: middle;
+                margin-right: 32px;
+                margin-bottom: 32px;
+            }
+        </style>
     </head>
     <body>
         <div style='margin-top: 50px; text-align: center'>
-            <div style="margin-bottom: 20px">
+            <div style="margin-bottom: 48px">
                 <a class="title" href="http://chalk.pe">
                     <?php
                         if(isset($_GET["name"])) echo "<span style=\"font-family: 'Nanum Pen Script'\">" . $_GET["name"] . "님 안녕!</span>";
@@ -15,31 +27,22 @@
                 </a>
             </div>
 
-            <div style='display: inline-block; vertical-align: middle; margin-right: 16px'>
-                <a href="/src/DiurnalCircles.html">
-                    <img src="/assets/sketch-preview/DiurnalCircles.png" width="165px" height="165px" />
-                    <br />
-                    Diurnal circles
-                </a>
-            </div>
-            <div style='display: inline-block; vertical-align: middle; margin-left: 16px'>
-                <a href="/src/SpiralBlocks.html">
-                    <img src="/assets/sketch-preview/SpiralBlocks.png" width="165px" height="165px" />
-                    <br />
-                    Spiral blocks</a>
-            </div>
-            <div style='display: inline-block; vertical-align: middle; margin-left: 16px'>
-                <a href="/src/Present.html">
-                    <img src="/assets/sketch-preview/Present.png" width="165px" height="165px" />
-                    <br />
-                    Present</a>
-            </div>
-            <div style='display: inline-block; vertical-align: middle; margin-left: 16px'>
-                <a href="/src/Triangle.html">
-                    <img src="/assets/sketch-preview/Triangle.png" width="165px" height="165px" />
-                    <br />
-                    Triangle</a>
-            </div>
+            <div class="preview"> <a href="/src/DiurnalCircles.html">
+                <img src="/assets/sketch-preview/DiurnalCircles.png" width="165px" height="165px" /> <br />
+                Diurnal circles
+            </a> </div> <div class="preview"> <a href="/src/SpiralBlocks.html">
+                <img src="/assets/sketch-preview/SpiralBlocks.png" width="165px" height="165px" /> <br />
+                Spiral blocks
+            </a> </div> <div class="preview"> <a href="/src/Present.html">
+                <img src="/assets/sketch-preview/Present.png" width="165px" height="165px" /> <br />
+                Present
+            </a> </div> <div class="preview"> <a href="/src/Triangle.html">
+                <img src="/assets/sketch-preview/Triangle.png" width="165px" height="165px" /> <br />
+                Triangle
+            </a> </div> <div class="preview"> <a href="/src/Group.html">
+                <img style="background: #607D8B" src="/assets/sketch-preview/Group.png" width="165px" height="165px" /> <br />
+                Group
+            </a> </div>
         </div>
     </body>
 </html>
